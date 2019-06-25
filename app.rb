144340dfb9809d15f2480eaf15f60dd4f972bdba
Haley DeKeyser <hcd09@live.com>
2019-06-25 18:30:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
   end
   
   post '/piglatinize' do
-    @piglatinizer = PigLatinizer.new(params[:user_phrase])
+    @piglatinizer = PigLatinizer.new(params["user_phrase"])
     
     erb :piglatinize
   end
