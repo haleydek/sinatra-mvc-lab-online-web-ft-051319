c90c8c87a7d949ec2e4cc08bc9b4775a412e0089
Haley DeKeyser <hcd09@live.com>
2019-06-25 16:58:38 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,7 +8,11 @@ class PigLatinizer
   def piglatinize(word)
     first_letter = word[0].downcase
     
-    if first_letter == [/aeiou/]
+    if first_letter == "a" || first_letter == "e" || first_letter == "i" || first_letter == "o" || first_letter == "u"
+      
+    else
+      
+    end
   end
   
   def split_sentence(string)
