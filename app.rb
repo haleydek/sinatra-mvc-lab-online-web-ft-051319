2e6753d4a0aff66bfbee37713ab4fdf62febfcdb
Haley DeKeyser <hcd09@live.com>
2019-06-25 17:50:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
   end
   
   post '/' do
-    @piglatinized_text = PigLatinizer.new(params["user_input"])
+    @piglatinizer = PigLatinizer.new(params["user_input"])
     
     erb :results
   end
