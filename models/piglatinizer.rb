6a3fdf09c280dfeb05e76081e1aba916855e3710
Haley DeKeyser <hcd09@live.com>
2019-06-25 16:48:04 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,15 @@
+class PigLatinizer
+  attr_accessor :string
+  
+  def initialize(string)
+    @string = string
+  end
+  
+  def piglatinize(word)
+    
+  end
+  
+  def split_sentence(string)
+    string.split(" ")
+  end
+end
