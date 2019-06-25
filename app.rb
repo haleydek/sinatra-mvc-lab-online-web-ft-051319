f7fa2aa6dea456578c27af45de83b8132a3b56dc
Haley DeKeyser <hcd09@live.com>
2019-06-25 17:45:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/' do
-    
+    erb :user_input
+  end
+  
+  post '/' do
+    @user_input = params["user_input"]
   end
 end
